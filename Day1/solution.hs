d17d3204844c190ed2222542eca76ea70a7c0884
aceituna <juanhorcre@gmail.com>
2024-12-02 00:16:56 +0100

    parte 2

--- Day1/solution.hs
+++ Day1/solution.hs
@@ -1,4 +1,5 @@
-import Data.List
+import Data.List (group, nub, sort)
+import Text.Parsec (parse)
 
 parseNumbers :: String -> [Int]
 parseNumbers input = map read (words input)
@@ -14,7 +15,15 @@ odds (x : y : ys) = y : odds ys
 differences :: [Int] -> [Int]
 differences numbers = map abs $ zipWith (+) (map negate $ sort $ evens numbers) (sort $ odds numbers)
 
+ocurrences :: Int -> [Int] -> Int
+ocurrences numero lista = length $ filter (== numero) lista
+
+disjointCounts :: [Int] -> [Int] -> [(Int, Int)]
+disjointCounts from list = [(n, ocurrences n list) | n <- from]
+
 main :: IO ()
 main = do
   input <- readFile "input1.txt"
   print $ sum $ differences $ parseNumbers input
+
+  print $ sum $ map (uncurry (*)) $ disjointCounts (evens $ parseNumbers input) (odds $ parseNumbers input)
