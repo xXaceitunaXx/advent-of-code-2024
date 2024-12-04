331ad188634ce787624e67e8d789deba2ef217b6
aceituna <juanhorcre@gmail.com>
2024-12-04 17:51:44 +0100

    parte 2 lol

--- Day4/solucion.hs
+++ Day4/solucion.hs
@@ -7,7 +7,7 @@ vertical :: String -> [String]
 vertical input = transpose $ horizontal input
 
 diagPos :: String -> [String]
-diagPos input = filter (\x -> length x > 3) $ diagonals $ horizontal input
+diagPos input = diagonals $ horizontal input
   where
     diagonals matrix = [ [matrix !! (i + k) !! k | k <- [0 .. min (n - i - 1) (m - 1)]] | i <- [0 .. (n - 1)] ] ++
                        [ [matrix !! k !! (j + k) | k <- [0 .. min (n - 1) (m - j - 1)]] | j <- [1 .. (m - 1)] ]
@@ -27,7 +27,23 @@ substringOcurrences subs string = length $ filter (isPrefixOf subs) (tails strin
 countXMAS :: [String] -> [Int]
 countXMAS list = zipWith (+) (map (substringOcurrences "XMAS") list) (map (substringOcurrences "SAMX") list)
 
+generateContainers :: String -> [[String]]
+generateContainers input = [[take 3 (drop j (matrix !! (i + k))) | k <- [0..2]] | i <- [0..n-3], j <- [0..m-3]]
+  where
+    matrix = horizontal input
+    n = length matrix
+    m = length $ head matrix
+
+validDiagonal :: [Char] -> Bool
+validDiagonal [x, y] = x == 'M' && y == 'S' || x == 'S' && y == 'M'
+
+validContainer :: [String] -> Bool
+validContainer container = container !! 1 !! 1 == 'A' &&
+                           validDiagonal [head (head container), container !! 2 !! 2] &&
+                           validDiagonal [head (container !! 2), head container !! 2]
+
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     print $ sum $ countXMAS $ concat [horizontal input, vertical input, diagPos input, diagNeg input]
+    print $ length $ filter validContainer $ generateContainers input
